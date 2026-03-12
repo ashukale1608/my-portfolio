2db03bdaa41cc756c1ac6731127cd2d61702bdb6
Ashwini kale <ashwini.kale@simplifyvms.com>
2026-03-12 15:59:04 +0530

    done-basic-setup

---
+++ src/home.page/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+    console.log("hii")
+  return (
+    <div style={{textAlign:"center", marginTop:"100px"}}>
+      <h1>Welcome 🎉</h1>
+      <h2>You are logged in successfully</h2>
+    </div>
+  );
+}
+
+export default Home;
